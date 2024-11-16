e155944598ca214949343770d0c70e0104b43421
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-11-16 21:48:28 +0300

    feat: created ValueObject model

--- Ecommerce.Domain/Common/Models/ValueObject.cs
+++ Ecommerce.Domain/Common/Models/ValueObject.cs
@@ -1,20 +1,18 @@
-public abstract class ValueObject<T> : IEquatable<ValueObject<T>>
+namespace Ecommerce.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
 {
-    public T Value { get; }
-
-    protected ValueObject(T value) => Value = value;
-
     public abstract IEnumerable<object> GetEqualityComponents();
 
     public override bool Equals(object? obj)
     {
-        if (obj is null || obj is not ValueObject<T> other) return false;
+        if (obj is null || obj is not ValueObject other) return false;
 
         return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
     }
 
-    public static bool operator ==(ValueObject<T> first, ValueObject<T> second) => first.Equals(second);
-    public static bool operator !=(ValueObject<T> first, ValueObject<T> second) => !first.Equals(second);
+    public static bool operator ==(ValueObject first, ValueObject second) => first.Equals(second);
+    public static bool operator !=(ValueObject first, ValueObject second) => !first.Equals(second);
 
 
     public override int GetHashCode()
@@ -24,5 +22,5 @@ public abstract class ValueObject<T> : IEquatable<ValueObject<T>>
             .Aggregate((x, y) => x ^ y);
     }
 
-    public bool Equals(ValueObject<T>? other) => other is not null && Equals((object)other);
+    public bool Equals(ValueObject? other) => other is not null && Equals((object)other);
 }
