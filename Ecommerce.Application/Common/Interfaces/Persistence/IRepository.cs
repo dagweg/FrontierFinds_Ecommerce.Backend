bd54de1a0554410b3901b4800c964aac1c9686ab
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-17 17:01:30 +0300

    feat: Defined Repository Interfaces

---
+++ Ecommerce.Application/Common/Interfaces/Persistence/IRepository.cs
@@ -0,0 +1,18 @@
+namespace Ecommerce.Application.Common.Interfaces.Persistence;
+
+/// <summary>
+/// This interface is used to define the basic CRUD operations for all ORM vendors.
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+/// <typeparam name="TId"></typeparam>
+
+public interface IRepository<TEntity, TId>
+    where TEntity : class
+{
+    Task<TEntity> GetByIdAsync(TId id);
+    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+    Task SaveChangesAsync();
+}
---
+++ Ecommerce.Application/Common/Interfaces/Persistence/IUserRespository.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce.Application.Common.Interfaces.Persistence;
+
+using Ecommerce.Domain.Common.ValueObjects;
+using Ecommerce.Domain.User;
+
+public interface IUserRespository : IRepository<User, Guid>
+{
+    Task<User> GetByEmailAsync(Email email);
+}
