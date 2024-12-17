9b4c9b5503054cf5a6c7ecafae710a349006955f
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-17 18:37:32 +0300

    feat: setup husky git-hook to run csharpier formatter

--- Ecommerce.Application/Common/Interfaces/Persistence/IRepository.cs
+++ Ecommerce.Application/Common/Interfaces/Persistence/IRepository.cs
@@ -10,6 +10,7 @@ public interface IRepository<TEntity, TId>
     where TEntity : class
 {
     Task<TEntity> GetByIdAsync(TId id);
+
     Task<IEnumerable<TEntity>> GetAllAsync();
     Task AddAsync(TEntity entity);
     Task UpdateAsync(TEntity entity);
