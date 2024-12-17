f65c4517212ea40edda86c5cfbf8106bc6b733ca
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-17 17:05:15 +0300

    feat: Injected MediatR inside AuthenticationController and Sent Register Command

--- Ecommerce.Presentation/Api/Controllers/AuthenticationController.cs
+++ Ecommerce.Presentation/Api/Controllers/AuthenticationController.cs
@@ -1,20 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
+namespace Ecommerce.Presentation.Api.Controllers;
 
-namespace Api.Controllers;
+using Contracts.Authentication;
+using Ecommerce.Application.UseCases.UserManagement.Authentication.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("auth")]
-public class AuthenticationController : ControllerBase
+public class AuthenticationController(IMediator mediator) : ControllerBase
 {
     [HttpPost("register")]
-    public Task<IActionResult> Register()
+    public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
     {
-        return Task.FromResult<IActionResult>(Ok("Registration successful"));
+        var command = new RegisterUserCommand(
+            registerRequest.FirstName,
+            registerRequest.LastName,
+            registerRequest.Email,
+            registerRequest.Password,
+            registerRequest.PhoneNumber,
+            registerRequest.CountryCode
+        );
+
+        var result = await mediator.Send(command);
+
+        return Ok(result);
     }
 
     [HttpPost("login")]
-    public Task<IActionResult> Login()
-    {
-        return Task.FromResult<IActionResult>(Ok("Login successful"));
-    }
+    public Task<IActionResult> Login() => Task.FromResult<IActionResult>(Ok("Login successful"));
 }
