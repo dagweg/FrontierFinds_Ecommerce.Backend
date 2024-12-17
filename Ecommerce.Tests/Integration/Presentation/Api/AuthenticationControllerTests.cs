a8254e888b065c15672755fb07faa04ce098f90a
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-17 17:07:46 +0300

    Initialized Auth Controller Unit and Integration Testing Environment

---
+++ Ecommerce.Tests/Integration/Presentation/Api/AuthenticationControllerTests.cs
@@ -0,0 +1,40 @@
+namespace Ecommerce.Tests.Integration.Presentation.Api;
+
+using System.Net;
+using System.Net.Http.Json;
+using global::Api;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+public class AuthenticationControllerTests(WebApplicationFactory<Program> webApplicationFactory)
+{
+    private readonly HttpClient _httpClient = webApplicationFactory.CreateClient();
+
+    [Fact]
+    public async Task POST_Login_WithValidCredentials_ReturnsOk()
+    {
+        var response = await _httpClient.PostAsJsonAsync<object>(
+            "/auth/login",
+            new { email = "sample@gmail.com", password = "password" }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task POST_Register_WithValidCredentials_ReturnsOk()
+    {
+        var response = await _httpClient.PostAsJsonAsync<object>(
+            "/auth/register",
+            new
+            {
+                firstName = "sampleFirstName",
+                lastName = "sampleLastName",
+                email = "sample@gmail.com",
+                password = "password",
+            }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
---
+++ Ecommerce.Tests/Presentation/Api/AuthenticationControllerTests.cs
@@ -0,0 +1,30 @@
+namespace Ecommerce.Tests.Presentation.Api;
+
+using Ecommerce.Presentation.Api.Controllers;
+using Ecommerce.Presentation.Contracts.Authentication;
+using Xunit.Abstractions;
+
+public class AuthenticationControllerTests(
+    ITestOutputHelper testOutputHelper,
+    AuthenticationController authenticationController
+)
+{
+    private readonly AuthenticationController _authenticationController = authenticationController;
+    private readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
+
+    [Fact]
+    private async Task Register_WithValidRequest_ReturnsOk()
+    {
+        var registerRequest = new RegisterRequest(
+            "firstName",
+            "lastName",
+            "email",
+            "password",
+            "phoneNumber",
+            1
+        );
+        var actionResult = await _authenticationController.Register(registerRequest);
+
+        _testOutputHelper.WriteLine(actionResult.ToString());
+    }
+}
