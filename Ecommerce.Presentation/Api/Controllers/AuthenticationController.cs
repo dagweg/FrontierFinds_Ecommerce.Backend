0c318b94b2914178315a3ab083c7b012f896e314
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-23 17:18:00 +0300

    feat: create login route controller

--- Ecommerce.Presentation/Api/Controllers/AuthenticationController.cs
+++ Ecommerce.Presentation/Api/Controllers/AuthenticationController.cs
@@ -1,7 +1,8 @@
 namespace Ecommerce.Presentation.Api.Controllers;
 
 using Contracts.Authentication;
-using Ecommerce.Application.UseCases.UserManagement.Authentication.Commands;
+using Ecommerce.Application.UseCases.Users.Commands.RegisterUser;
+using Ecommerce.Application.UseCases.Users.Queries.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,23 +10,30 @@ using Microsoft.AspNetCore.Mvc;
 [Route("auth")]
 public class AuthenticationController(IMediator mediator) : ControllerBase
 {
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
-    {
-        var command = new RegisterUserCommand(
-            registerRequest.FirstName,
-            registerRequest.LastName,
-            registerRequest.Email,
-            registerRequest.Password,
-            registerRequest.PhoneNumber,
-            registerRequest.CountryCode
-        );
+  [HttpPost("register")]
+  public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+  {
+    var command = new RegisterUserCommand(
+      registerRequest.FirstName,
+      registerRequest.LastName,
+      registerRequest.Email,
+      registerRequest.Password,
+      registerRequest.PhoneNumber,
+      registerRequest.CountryCode
+    );
 
-        var result = await mediator.Send(command);
+    var result = await mediator.Send(command);
 
-        return Ok(result);
-    }
+    return Ok(result);
+  }
 
-    [HttpPost("login")]
-    public Task<IActionResult> Login() => Task.FromResult<IActionResult>(Ok("Login successful"));
+  [HttpPost("login")]
+  public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+  {
+    var loginQuery = new LoginUserQuery(loginRequest.Email, loginRequest.Password);
+
+    var result = await mediator.Send(loginQuery);
+
+    return Ok(result);
+  }
 }
