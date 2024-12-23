284603ec85b53e92ff73739d626c17ba35427dcb
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-23 17:17:17 +0300

    chore: update Guid to UserId

--- Ecommerce.Domain/UserAggregate/User.cs
+++ Ecommerce.Domain/UserAggregate/User.cs
@@ -1,33 +1,32 @@
 namespace Ecommerce.Domain.User;
 
-using System.Runtime.InteropServices;
 using Ecommerce.Domain.Common.Models;
 using Ecommerce.Domain.Common.ValueObjects;
 
-public class User : AggregateRoot<Guid>
+public class User : AggregateRoot<UserId>
 {
-    public Name FirstName { get; private set; } = Name.Empty;
-    public Name LastName { get; private set; } = Name.Empty;
-    public Email Email { get; private set; } = Email.Empty;
-    public Password Password { get; private set; } = Password.Empty;
+  public Name FirstName { get; private set; } = Name.Empty;
+  public Name LastName { get; private set; } = Name.Empty;
+  public Email Email { get; private set; } = Email.Empty;
+  public Password Password { get; private set; } = Password.Empty;
 
-    private User()
-        : base(Guid.NewGuid()) { }
+  private User()
+    : base(UserId.CreateUnique()) { }
 
-    private User(Guid id, Name firstName, Name lastName, Email email, Password password)
-        : base(id)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-        Password = password;
-    }
+  private User(UserId id, Name firstName, Name lastName, Email email, Password password)
+    : base(id)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+    Email = email;
+    Password = password;
+  }
 
-    public static User Create(Name firstName, Name lastName, Email email, Password password) =>
-        new User(Guid.NewGuid(), firstName, lastName, email, password);
+  public static User Create(Name firstName, Name lastName, Email email, Password password) =>
+    new(UserId.CreateUnique(), firstName, lastName, email, password);
 
-    public override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Id;
-    }
+  public override IEnumerable<object> GetEqualityComponents()
+  {
+    yield return Id;
+  }
 }
