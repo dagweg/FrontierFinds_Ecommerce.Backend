84668fcdcb5219b62245ae80f4696d38f9be39e5
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-23 17:16:31 +0300

    feat: added Empty, CreateUnique and an implicit conversion to string operator

--- Ecommerce.Domain/Common/ValueObjects/UserId.cs
+++ Ecommerce.Domain/Common/ValueObjects/UserId.cs
@@ -4,17 +4,24 @@ using Ecommerce.Domain.Common.Models;
 
 public sealed class UserId : ValueObject
 {
-    private UserId(Guid value)
-    {
-        Value = value;
-    }
+  private UserId(Guid value)
+  {
+    Value = value;
+  }
 
-    public Guid Value { get; }
+  public Guid Value { get; }
 
-    public static UserId CreateUnique() => new UserId(Guid.NewGuid());
+  public static Guid Empty => Guid.Empty;
 
-    public override IEnumerable<object> GetEqualityComponents()
-    {
-        yield return Value;
-    }
+  public static UserId CreateUnique() => new(Guid.NewGuid());
+
+  public override IEnumerable<object> GetEqualityComponents()
+  {
+    yield return Value;
+  }
+
+  public static implicit operator string(UserId userId)
+  {
+    return userId.Value.ToString();
+  }
 }
