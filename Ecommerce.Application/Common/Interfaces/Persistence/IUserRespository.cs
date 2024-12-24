8567075c7125a2665a8f070881f93d6666966ba5
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-24 04:08:29 +0300

    cleanup

--- Ecommerce.Application/Common/Interfaces/Persistence/IUserRespository.cs
+++
@@ -1,9 +0,0 @@
-namespace Ecommerce.Application.Common.Interfaces.Persistence;
-
-using Ecommerce.Domain.Common.ValueObjects;
-using Ecommerce.Domain.User;
-
-public interface IUserRespository : IRepository<User, Guid>
-{
-  Task<User?> GetByEmailAsync(Email email);
-}
