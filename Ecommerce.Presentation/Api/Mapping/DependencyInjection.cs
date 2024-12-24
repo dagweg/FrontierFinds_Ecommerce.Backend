e9068fb02bca18970e0ea21cba23b0b5cd186dd3
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-25 00:47:04 +0300

    fix: registered automapper maps to di for app and api

--- Ecommerce.Presentation/Api/Mapping/DependencyInjection.cs
+++ Ecommerce.Presentation/Api/Mapping/DependencyInjection.cs
@@ -4,7 +4,13 @@ public static class DependencyInjection
 {
   public static IServiceCollection ConfigureAutomapper(this IServiceCollection services)
   {
-    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    services.AddAutoMapper(
+      (cfg) =>
+      {
+        cfg.AddMaps(typeof(Application.DependencyInjection).Assembly);
+        cfg.AddMaps(typeof(Api.DependencyInjection).Assembly);
+      }
+    );
     return services;
   }
 }
