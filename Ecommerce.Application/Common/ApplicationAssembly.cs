0d9aa640305bf0e4fb6c0b9f6f881b7a98a7cc84
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-25 00:44:03 +0300

    created application assembly for ease of use

---
+++ Ecommerce.Application/Common/ApplicationAssembly.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+namespace Ecommerce.Application.Common;
+
+// Helper class for Accessing the Application Assembly
+public static class ApplicationAssembly
+{
+  public static Assembly Assembly => typeof(ApplicationAssembly).Assembly;
+}
