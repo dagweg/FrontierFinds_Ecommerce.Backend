7ba52ff466d178b98d1f0d4adfc452f066b0a622
Dagmawi Wegayehu <dagtef2@gmail.com>
2024-12-28 02:40:37 +0300

    feat: entity-framework core design time factory for creating migrations

---
+++ Ecommerce.Infrastructure/Persistence/EfCore/EfCoreContextFactory.cs
@@ -0,0 +1,23 @@
+using Ecommerce.Infrastructure.Persistence.EfCore.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure.Persistence.EfCore;
+
+public class EfCoreContextFactory : IDesignTimeDbContextFactory<EfCoreContext>
+{
+  public EfCoreContext CreateDbContext(string[] args)
+  {
+    var optionsBuilder = new DbContextOptionsBuilder<EfCoreContext>();
+
+    var configuration = new ConfigurationBuilder().Build();
+
+    optionsBuilder.UseSqlServer(
+      "Server=EVOO-EG-LP7\\SQLEXPRESS;Database=ecommerce;Trusted_Connection=True;TrustServerCertificate=True;"
+    );
+
+    return new EfCoreContext(optionsBuilder.Options);
+  }
+}
