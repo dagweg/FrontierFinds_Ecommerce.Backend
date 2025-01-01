e6c745f73a355c55779962c54fa7947a270767ce
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-01 05:00:40 +0300

    added tables to efcorecontext

--- Ecommerce.Infrastructure/Persistence/EfCore/EfCoreContext.cs
+++ Ecommerce.Infrastructure/Persistence/EfCore/EfCoreContext.cs
@@ -1,3 +1,6 @@
+using Ecommerce.Domain.Common.ValueObjects;
+using Ecommerce.Domain.ProductAggregate;
+using Ecommerce.Domain.ProductAggregate.Entities;
 using Ecommerce.Domain.UserAggregate;
 using Ecommerce.Infrastructure.Common;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +10,11 @@ namespace Ecommerce.Infrastructure.Persistence.EfCore;
 public class EfCoreContext : DbContext
 {
   public DbSet<User> Users { get; set; } = null!;
+  public DbSet<Product> Products { get; set; } = null!;
+  public DbSet<ProductCategory> ProductCategories { get; set; } = null!;
+  public DbSet<Promotion> Promotions { get; set; } = null!;
+
+  public EfCoreContext() { }
 
   public EfCoreContext(DbContextOptions<EfCoreContext> options)
     : base(options) { }
@@ -16,9 +24,9 @@ public class EfCoreContext : DbContext
     base.OnConfiguring(optionsBuilder);
   }
 
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  protected override void OnModelCreating(ModelBuilder builder)
   {
     // Loads in the configurations that implement the IEntityTypeConfiguration and others
-    modelBuilder.ApplyConfigurationsFromAssembly(InfrastructureAssembly.Assembly);
+    builder.ApplyConfigurationsFromAssembly(InfrastructureAssembly.Assembly);
   }
 }
