3a467e73997a196978d16afd4ce1d669c081b3dc
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-01 21:59:35 +0300

    feat: address vo

--- Ecommerce.Domain/Common/ValueObjects/Address.cs
+++ Ecommerce.Domain/Common/ValueObjects/Address.cs
@@ -10,6 +10,28 @@ public class Address : ValueObject
   public string Country { get; private set; } = string.Empty;
   public string ZipCode { get; private set; } = string.Empty;
 
+  protected Address() { }
+
+  protected Address(string street, string city, string state, string country, string zipCode)
+  {
+    Street = street;
+    City = city;
+    State = state;
+    Country = country;
+    ZipCode = zipCode;
+  }
+
+  public static Address Create(
+    string street,
+    string city,
+    string state,
+    string country,
+    string zipCode
+  )
+  {
+    return new Address(street, city, state, country, zipCode);
+  }
+
   public override IEnumerable<object> GetEqualityComponents()
   {
     yield return Street;
