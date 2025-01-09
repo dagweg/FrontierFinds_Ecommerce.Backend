5ff0ebab222426970e39c2a05e34d9c5c9db1706
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-09 14:17:54 +0300

    feat: localization and authentication registered to di cont.

--- Ecommerce.Infrastructure/DependencyInjection.cs
+++ Ecommerce.Infrastructure/DependencyInjection.cs
@@ -1,19 +1,26 @@
 namespace Ecommerce.Infrastructure;
 
-using Ecommerce.Application.Common.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Ecommerce.Application.Common;
 using Ecommerce.Application.Common.Interfaces.Authentication;
 using Ecommerce.Application.Common.Interfaces.Persistence;
-using Ecommerce.Infrastructure.Common;
+using Ecommerce.Application.Common.Interfaces.Providers.Date;
+using Ecommerce.Application.Common.Interfaces.Providers.Localization;
+using Ecommerce.Infrastructure.Common.Providers;
+using Ecommerce.Infrastructure.Common.Providers.Localization;
 using Ecommerce.Infrastructure.Persistence.EfCore;
 using Ecommerce.Infrastructure.Persistence.EfCore.Interceptors;
 using Ecommerce.Infrastructure.Persistence.EfCore.Options;
 using Ecommerce.Infrastructure.Persistence.EfCore.Repositories;
 using Ecommerce.Infrastructure.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 
 public static class DependencyInjection
@@ -24,9 +31,13 @@ public static class DependencyInjection
     IConfigurationManager configuration
   )
   {
+    services.AddAuth(configuration);
+
     services.AddScoped<PublishDomainEventsInterceptor>();
 
     services.AddPersistence(configuration);
+
+    services.AddLocalization();
 
     // Load settings from appsettings.json to appropriate classes
     services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
@@ -34,7 +45,6 @@ public static class DependencyInjection
     // Register Utility Services
     services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
     services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
-    services.AddScoped<IValidationMessageProvider, ValidationMessageProvider>();
 
     return services;
   }
@@ -52,9 +62,7 @@ public static class DependencyInjection
       {
         // Connect to SqlServer using the connection string.
         options
-          .UseSqlServer(
-            "Server=EVOO-EG-LP7\\SQLEXPRESS;Database=ecommerce;Trusted_Connection=True;TrustServerCertificate=True;"
-          )
+          .UseSqlServer(configuration[$"{SqlServerOptions.SectionName}:ConnectionString"])
           .AddInterceptors(sp.GetRequiredService<PublishDomainEventsInterceptor>())
           .EnableDetailedErrors(true);
       }
@@ -76,5 +84,50 @@ public static class DependencyInjection
       }
     );
     return hostBuilder;
+  }
+
+  public static IServiceCollection AddLocalization(this IServiceCollection services)
+  {
+    services.AddScoped<IValidationMessages>(_ => new ValidationMessageProvider(
+      "Ecommerce.Application.Common.Resources.ValidationMessages", // path to the resource (.resx) file
+      ApplicationAssembly.Assembly
+    ));
+
+    services.AddScoped<IAuthenticationMessages>(_ => new AuthenticationMessageProvider(
+      "Ecommerce.Application.Common.Resources.AuthenticationMessages",
+      ApplicationAssembly.Assembly
+    ));
+
+    return services;
+  }
+
+  public static IServiceCollection AddAuth(
+    this IServiceCollection services,
+    IConfigurationManager configuration
+  )
+  {
+    services
+      .AddAuthentication("Bearer")
+      .AddJwtBearer(options =>
+      {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidateLifetime = true,
+          ValidateIssuerSigningKey = true,
+          ValidIssuer = configuration[$"{JwtSettings.SectionName}:{nameof(JwtSettings.Issuer)}"],
+          ValidAudience = configuration[
+            $"{JwtSettings.SectionName}:{nameof(JwtSettings.Audience)}"
+          ],
+          IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(
+              configuration[$"{JwtSettings.SectionName}:{nameof(JwtSettings.SecretKey)}"]
+                ?? throw new InvalidOperationException("Jwt Secret key is null!")
+            )
+          ),
+        };
+      });
+    return services;
   }
 }
