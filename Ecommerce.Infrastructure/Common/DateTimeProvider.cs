a01676749070c79ffa0ddaf50a602da7f85eb24d
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-09 04:23:16 +0300

    moved

--- Ecommerce.Infrastructure/Common/DateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Ecommerce.Application.Common.Interfaces;
-
-namespace Ecommerce.Infrastructure.Common;
-
-public class DateTimeProvider : IDateTimeProvider
-{
-  public DateTime UtcNow { get; set; } = DateTime.UtcNow;
-}
