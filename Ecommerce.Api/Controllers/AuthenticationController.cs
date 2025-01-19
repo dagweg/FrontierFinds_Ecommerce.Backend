53cea899946770ac22d238445cd7ff02f187c907
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-19 21:37:02 +0300

    feat: login sets jwt token as http-only cookie

--- Ecommerce.Api/Controllers/AuthenticationController.cs
+++ Ecommerce.Api/Controllers/AuthenticationController.cs
@@ -1,11 +1,14 @@
 namespace Ecommerce.Api.Controllers;
 
+using System.IdentityModel.Tokens.Jwt;
 using AutoMapper;
 using Ecommerce.Application.UseCases.Users.Commands.RegisterUser;
 using Ecommerce.Application.UseCases.Users.Queries.LoginUser;
 using Ecommerce.Contracts.Authentication;
+using Ecommerce.Infrastructure.Services.Authentication;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 [ApiController]
 [Route("auth")]
@@ -13,11 +16,23 @@ public class AuthenticationController : ControllerBase
 {
   private readonly ISender _mediator;
   private readonly IMapper _mapper;
+  private readonly CookieSettings _cookieSettings;
+  private readonly IWebHostEnvironment _webHostEnv;
+  private readonly ILogger<AuthenticationController> _logger;
 
-  public AuthenticationController(ISender mediator, IMapper mapper)
+  public AuthenticationController(
+    ISender mediator,
+    IMapper mapper,
+    IOptions<CookieSettings> cookieSettings,
+    IWebHostEnvironment webHostEnv,
+    ILogger<AuthenticationController> logger
+  )
   {
     _mediator = mediator;
     _mapper = mapper;
+    _cookieSettings = cookieSettings.Value;
+    _webHostEnv = webHostEnv;
+    _logger = logger;
   }
 
   [HttpPost("register")]
@@ -39,8 +54,34 @@ public class AuthenticationController : ControllerBase
 
     var result = await _mediator.Send(loginQuery);
 
-    return result.IsFailed
-      ? new ObjectResult(result)
-      : Ok(_mapper.Map<AuthenticationResponse>(result.Value));
+    if (result.IsFailed)
+      return new ObjectResult(result);
+
+    var jwtToken = new JwtSecurityTokenHandler().ReadJwtToken(result.Value.Token);
+    var expClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Exp);
+
+    if (expClaim is null)
+    {
+      _logger.LogError(
+        "Token does not contain an expiration claim. Token: {Token}",
+        result.Value.Token
+      );
+      return StatusCode(500, "An error occured trying to log you in. Please contact support.");
+    }
+
+    // Set the http-only cookie
+    HttpContext.Response.Cookies.Append(
+      _cookieSettings.CookieKey,
+      result.Value.Token,
+      new CookieOptions
+      {
+        HttpOnly = _cookieSettings.HttpOnly,
+        Secure = _cookieSettings.Secure,
+        Expires = DateTimeOffset.FromUnixTimeSeconds(long.Parse(expClaim.Value)),
+        SameSite = _cookieSettings.SameSite,
+      }
+    );
+
+    return Ok(_mapper.Map<AuthenticationResponse>(result.Value));
   }
 }
