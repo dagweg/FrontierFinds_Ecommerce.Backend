9077686ad12c2220593b7b44c8518231eac798f5
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-23 01:34:53 +0300

    changed from imageResult to return url instead

--- Ecommerce.Application/UseCases/Products/Common/ProductImagesResult.cs
+++ Ecommerce.Application/UseCases/Products/Common/ProductImagesResult.cs
@@ -5,9 +5,9 @@ namespace Ecommerce.Application.UseCases.Products.Common;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 public class ProductImagesResult
 {
-  public ImageResult? LeftImage { get; set; }
-  public ImageResult? RightImage { get; set; }
-  public ImageResult? FrontImage { get; set; }
-  public ImageResult? BackImage { get; set; }
+  public string? LeftImageUrl { get; set; }
+  public string? RightImageUrl { get; set; }
+  public string? FrontImageUrl { get; set; }
+  public string? BackImageUrl { get; set; }
 }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
