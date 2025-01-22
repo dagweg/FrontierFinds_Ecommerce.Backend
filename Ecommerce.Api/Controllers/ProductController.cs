b728381b4675dd44f81bf3f7b9568fb92c670c4f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-23 01:30:36 +0300

    handle fail case

--- Ecommerce.Api/Controllers/ProductController.cs
+++ Ecommerce.Api/Controllers/ProductController.cs
@@ -14,11 +14,13 @@ public class ProductController : ControllerBase
 {
   private readonly ISender _mediator;
   private readonly IMapper _mapper;
+  private readonly ILogger<ProductController> _logger;
 
-  public ProductController(ISender mediator, IMapper mapper)
+  public ProductController(ISender mediator, IMapper mapper, ILogger<ProductController> logger)
   {
     _mediator = mediator;
     _mapper = mapper;
+    _logger = logger;
   }
 
   [HttpPost]
@@ -27,6 +29,11 @@ public class ProductController : ControllerBase
   )
   {
     var result = await _mediator.Send(_mapper.Map<CreateProductCommand>(createProductRequest));
+    if (result.IsFailed)
+    {
+      _logger.LogError("Failed to create product: {@result}", result);
+      return new ObjectResult(result);
+    }
     return Ok(_mapper.Map<ProductResponse>(result.Value));
   }
 }
