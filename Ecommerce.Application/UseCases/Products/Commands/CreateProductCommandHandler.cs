8596113092da7d7d362d0042eaedd358ef94cf40
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-23 01:33:58 +0300

    feat: create product command handler

--- Ecommerce.Application/UseCases/Products/Commands/CreateProductCommandHandler.cs
+++ Ecommerce.Application/UseCases/Products/Commands/CreateProductCommandHandler.cs
@@ -1,10 +1,19 @@
 using AutoMapper;
+using Ecommerce.Application.Common.Errors;
+using Ecommerce.Application.Common.Interfaces.Persistence;
+using Ecommerce.Application.Common.Interfaces.Providers.Context;
+using Ecommerce.Application.Common.Utilities;
 using Ecommerce.Application.UseCases.Images.Common;
 using Ecommerce.Application.UseCases.Products.Common;
 using Ecommerce.Domain.Common.Entities;
 using Ecommerce.Domain.Common.ValueObjects;
+using Ecommerce.Domain.ProductAggregate;
+using Ecommerce.Domain.ProductAggregate.Entities;
+using Ecommerce.Domain.ProductAggregate.ValueObjects;
+using Ecommerce.Domain.UserAggregate.ValueObjects;
 using FluentResults;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.Application.UseCases.Products.Commands;
 
@@ -12,13 +21,27 @@ public class CreateProductCommandHandler
   : IRequestHandler<CreateProductCommand, Result<ProductResult>>
 {
   private readonly IMapper _mapper;
+  private readonly IProductRepository _productRepository;
+  private readonly IUnitOfWork _unitOfWork;
+  private readonly IUserContextService _userContextService;
+  private readonly ILogger<CreateProductCommandHandler> _logger;
 
-  public CreateProductCommandHandler(IMapper mapper)
+  public CreateProductCommandHandler(
+    IMapper mapper,
+    IProductRepository productRepository,
+    IUnitOfWork unitOfWork,
+    IUserContextService userContextService,
+    ILogger<CreateProductCommandHandler> logger
+  )
   {
     _mapper = mapper;
+    _productRepository = productRepository;
+    _unitOfWork = unitOfWork;
+    _userContextService = userContextService;
+    _logger = logger;
   }
 
-  public Task<Result<ProductResult>> Handle(
+  public async Task<Result<ProductResult>> Handle(
     CreateProductCommand command,
     CancellationToken cancellationToken
   )
@@ -30,26 +53,42 @@ public class CreateProductCommandHandler
 
       var images = new ProductImagesResult();
 
+      var name = ProductName.Create(command.ProductName);
+      var description = ProductDescription.Create(command.ProductDescription);
+      var stock = Stock.Create(Quantity.Create(command.StockQuantity));
       var currency = Price.ToCurrency(command.PriceCurrency);
+      var price = Price.Create(command.PriceValue, currency);
 
-      // var product = Product.Create(ProductName.Create(command.ProductName), ProductDescription.Create(command.ProductDescription), Price.Create(command.PriceValue, command.PriceCurrency)
-      var product = new ProductResult
+      var sellerId = _userContextService.GetUserId();
+      if (sellerId == null || Guid.TryParse(sellerId, out var sellerIdGuid) == false)
       {
-        ProductId = "1",
-        ProductName = command.ProductName,
-        ProductDescription = command.ProductDescription,
-        StockQuantity = command.StockQuantity,
-        PriceValue = command.PriceValue,
-        PriceCurrency = command.PriceCurrency,
-        Thumbnail = thumb,
-        Images = images,
-      };
+        return Result.Fail(new AuthenticationError("Cookie", "Not Logged In"));
+      }
 
-      return Task.FromResult(Result.Ok(product));
+      // TODO: Have CloudStorageService to upload image given by (command.thumbnail) and return the URL
+      var cloudUrlForThumbnail = "https://aws.s3.com/1.jpg";
+      var thumbnail = ProductImage.Create(cloudUrlForThumbnail);
+
+      var product = Product.Create(
+        name,
+        description,
+        price,
+        stock,
+        UserId.Convert(sellerIdGuid),
+        thumbnail
+      );
+
+      // add to repo
+      await _productRepository.AddAsync(product);
+
+      // persist
+      await _unitOfWork.SaveChangesAsync();
+
+      return Result.Ok(_mapper.Map<ProductResult>(product));
     }
     catch (Exception ex)
     {
-      Console.WriteLine(ex);
+      _logger.LogFormattedError(ex, ex.StackTrace ?? "No stack trace available");
       throw;
     }
   }
