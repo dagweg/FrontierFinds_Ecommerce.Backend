cea8142f6b7da639dcd42da628caf9f4a1bd3dc2
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-01-23 01:31:31 +0300

    register http accessor and context service

--- Ecommerce.Api/DependencyInjection.cs
+++ Ecommerce.Api/DependencyInjection.cs
@@ -1,6 +1,8 @@
 namespace Ecommerce.Api;
 
 using Ecommerce.Api.ActionFilters;
+using Ecommerce.Api.Services;
+using Ecommerce.Application.Common.Interfaces.Providers.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
@@ -15,12 +17,17 @@ public static class DependencyInjection
       options.Filters.Add<ModelBindingErrorActionFilter>();
     });
 
+    services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
     services.Configure<ApiBehaviorOptions>(options =>
     {
       options.SuppressModelStateInvalidFilter = true;
     });
 
     services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
+
+    services.AddScoped<IUserContextService, UserContextService>();
+
     return services;
   }
 }
