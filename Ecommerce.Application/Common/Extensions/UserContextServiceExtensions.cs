58fc4df97a601aeec00c8dc17d026374d6936a91
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:53:55 +0300

    validation method for `userId`

--- Ecommerce.Application/Common/Extensions/UserContextServiceExtensions.cs
+++ Ecommerce.Application/Common/Extensions/UserContextServiceExtensions.cs
@@ -1,26 +1,23 @@
 using Ecommerce.Application.Common.Errors;
+using Ecommerce.Application.Common.Interfaces.Persistence;
 using Ecommerce.Application.Common.Interfaces.Providers.Context;
 using Ecommerce.Domain.UserAggregate.ValueObjects;
 using FluentResults;
 
 namespace Ecommerce.Application.Common.Extensions;
 
-public static class userContextServiceExtensions
+public static class UserContextServiceExtensions
 {
   /// <summary>
   /// Get the current user's ID
   /// </summary>
   /// <param name="userContextService"></param>
-  /// <returns>
-  ///   A Result of UserId.
-  ///   If the user is not logged in, it will return an error
-  /// </returns>
   public static Result<UserId> GetValidUserId(this IUserContextService userContextService)
   {
     var sellerId = userContextService.GetUserId();
-    if (sellerId == null || Guid.TryParse(sellerId, out var sellerIdGuid) == false)
+    if (sellerId.IsFailed || Guid.TryParse(sellerId.Value, out var sellerIdGuid) == false)
     {
-      return ValidationErrors.AuthenticationError("Cookie", "Not Logged In");
+      return sellerId.ToResult();
     }
 
     return Result.Ok(UserId.Convert(sellerIdGuid));
