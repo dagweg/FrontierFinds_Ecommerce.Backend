19c169101a7a64f4fe410f22cd5085411a5b3a72
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:07:34 +0300

    feat: 'wishlists' result object

---
+++ Ecommerce.Application/UseCases/Users/Common/WishlistsResult.cs
@@ -0,0 +1,9 @@
+using Ecommerce.Application.UseCases.Products.Common;
+
+namespace Ecommerce.Application.UseCases.Users.Common;
+
+public class WishlistsResult
+{
+  public required int TotalItems { get; set; }
+  public IEnumerable<ProductResult> Wishlists { get; set; } = [];
+}
