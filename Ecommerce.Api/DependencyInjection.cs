4259aa701a42026540b76396f3699cb9f918ba86
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:42:55 +0300

    inject exception handlers

--- Ecommerce.Api/DependencyInjection.cs
+++ Ecommerce.Api/DependencyInjection.cs
@@ -1,8 +1,11 @@
 namespace Ecommerce.Api;
 
 using Ecommerce.Api.ActionFilters;
+using Ecommerce.Api.Exceptions;
+using Ecommerce.Api.Exceptions.Handlers;
 using Ecommerce.Api.Services;
 using Ecommerce.Application.Common.Interfaces.Providers.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
@@ -10,6 +13,8 @@ public static class DependencyInjection
 {
   public static IServiceCollection AddApi(this IServiceCollection services)
   {
+    services.AddExceptionHandlers();
+
     services.AddControllers(options =>
     {
       // Register global action filters
@@ -18,15 +23,24 @@ public static class DependencyInjection
     });
 
     services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+    services.AddTransient(typeof(IPasswordHasher<>), typeof(PasswordHasher<>));
 
     services.Configure<ApiBehaviorOptions>(options =>
     {
       options.SuppressModelStateInvalidFilter = true;
     });
 
-    services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
-
     services.AddScoped<IUserContextService, UserContextService>();
+
+    return services;
+  }
+
+  public static IServiceCollection AddExceptionHandlers(this IServiceCollection services)
+  {
+    services.AddSingleton<ExceptionHandlerFactory>();
+    services.AddSingleton<KeyNotFoundExceptionHandler>();
+    services.AddSingleton<UnauthorizedAccessExceptionHandler>();
+    services.AddSingleton<GenericExceptionHandler>();
 
     return services;
   }
