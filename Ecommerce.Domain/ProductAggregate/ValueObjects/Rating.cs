8f24b99e27fc25532b12c4682dc457ea7e53792e
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:49:43 +0300

    removed literal, introduced default state 'CreateEmpty' method

--- Ecommerce.Domain/ProductAggregate/ValueObjects/Rating.cs
+++ Ecommerce.Domain/ProductAggregate/ValueObjects/Rating.cs
@@ -1,4 +1,6 @@
+using Ecommerce.Domain.Common.Errors;
 using Ecommerce.Domain.Common.Models;
+using FluentResults;
 
 namespace Ecommerce.Domain.ProductAggregate.ValueObjects;
 
@@ -8,22 +10,29 @@ public sealed class Rating : ValueObject
 
   public static Rating Empty => new(0);
 
+  private const decimal MIN_RATING = 0;
+  private const decimal MAX_RATING = 5;
+
   private Rating() { }
 
   private Rating(decimal value)
   {
-    if (value < 0 || value > 5)
-    {
-      throw new ArgumentOutOfRangeException(nameof(value), "Rating must be between 0 and 5.");
-    }
-
     Value = value;
   }
 
-  public static Rating Create(decimal value)
+  public static Result<Rating> Create(decimal value)
   {
+    if (value < MIN_RATING || value > MAX_RATING)
+      return OutOfBoundsError<decimal>.GetResult(
+        nameof(value),
+        "Out of bounds.",
+        MIN_RATING,
+        MAX_RATING
+      );
     return new Rating(value);
   }
+
+  public static Rating CreateEmpty() => new(0);
 
   public static implicit operator decimal(Rating rating) => rating.Value;
 
