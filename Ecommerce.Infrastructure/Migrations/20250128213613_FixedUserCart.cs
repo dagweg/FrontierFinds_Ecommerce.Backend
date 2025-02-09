d01b0fd2fdbf906752a04845969a3b5d2f9efa4e
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:35:47 +0300

    migration

---
+++ Ecommerce.Infrastructure/Migrations/20250128213613_FixedUserCart.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+  /// <inheritdoc />
+  public partial class FixedUserCart : Migration
+  {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropIndex(name: "IX_UserCarts_UserId", table: "UserCarts");
+
+      migrationBuilder.CreateIndex(
+        name: "IX_UserCarts_UserId",
+        table: "UserCarts",
+        column: "UserId",
+        unique: true
+      );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropIndex(name: "IX_UserCarts_UserId", table: "UserCarts");
+
+      migrationBuilder.CreateIndex(
+        name: "IX_UserCarts_UserId",
+        table: "UserCarts",
+        column: "UserId"
+      );
+    }
+  }
+}
---
+++ Ecommerce.Infrastructure/Migrations/20250201201956_test-migration.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+  /// <inheritdoc />
+  public partial class testmigration : Migration
+  {
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_ZipCode",
+        table: "Users",
+        type: "nvarchar(10)",
+        maxLength: 10,
+        nullable: true,
+        oldClrType: typeof(string),
+        oldType: "nvarchar(10)",
+        oldMaxLength: 10
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_Street",
+        table: "Users",
+        type: "nvarchar(255)",
+        maxLength: 255,
+        nullable: true,
+        oldClrType: typeof(string),
+        oldType: "nvarchar(255)",
+        oldMaxLength: 255
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_State",
+        table: "Users",
+        type: "nvarchar(100)",
+        maxLength: 100,
+        nullable: true,
+        oldClrType: typeof(string),
+        oldType: "nvarchar(100)",
+        oldMaxLength: 100
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_Country",
+        table: "Users",
+        type: "nvarchar(max)",
+        nullable: true,
+        oldClrType: typeof(string),
+        oldType: "nvarchar(max)"
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_City",
+        table: "Users",
+        type: "nvarchar(100)",
+        maxLength: 100,
+        nullable: true,
+        oldClrType: typeof(string),
+        oldType: "nvarchar(100)",
+        oldMaxLength: 100
+      );
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_ZipCode",
+        table: "Users",
+        type: "nvarchar(10)",
+        maxLength: 10,
+        nullable: false,
+        defaultValue: "",
+        oldClrType: typeof(string),
+        oldType: "nvarchar(10)",
+        oldMaxLength: 10,
+        oldNullable: true
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_Street",
+        table: "Users",
+        type: "nvarchar(255)",
+        maxLength: 255,
+        nullable: false,
+        defaultValue: "",
+        oldClrType: typeof(string),
+        oldType: "nvarchar(255)",
+        oldMaxLength: 255,
+        oldNullable: true
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_State",
+        table: "Users",
+        type: "nvarchar(100)",
+        maxLength: 100,
+        nullable: false,
+        defaultValue: "",
+        oldClrType: typeof(string),
+        oldType: "nvarchar(100)",
+        oldMaxLength: 100,
+        oldNullable: true
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_Country",
+        table: "Users",
+        type: "nvarchar(max)",
+        nullable: false,
+        defaultValue: "",
+        oldClrType: typeof(string),
+        oldType: "nvarchar(max)",
+        oldNullable: true
+      );
+
+      migrationBuilder.AlterColumn<string>(
+        name: "Address_City",
+        table: "Users",
+        type: "nvarchar(100)",
+        maxLength: 100,
+        nullable: false,
+        defaultValue: "",
+        oldClrType: typeof(string),
+        oldType: "nvarchar(100)",
+        oldMaxLength: 100,
+        oldNullable: true
+      );
+    }
+  }
+}
