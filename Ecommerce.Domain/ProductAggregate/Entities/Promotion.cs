39ca9f91c2893f2bbe34327ff56c4bf104a36d11
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:48:32 +0300

    feat: implemented promotion blogic

--- Ecommerce.Domain/ProductAggregate/Entities/Promotion.cs
+++ Ecommerce.Domain/ProductAggregate/Entities/Promotion.cs
@@ -1,12 +1,17 @@
+using Ecommerce.Domain.Common.Errors;
 using Ecommerce.Domain.Common.Models;
+using FluentResults;
 
 namespace Ecommerce.Domain.ProductAggregate.Entities;
 
 public sealed class Promotion : Entity<Guid>
 {
-  public int DiscountPercentage { get; set; }
-  public DateTime StartDate { get; set; }
-  public DateTime EndDate { get; set; }
+  public int DiscountPercentage { get; init; }
+  public DateTime StartDate { get; init; }
+  public DateTime EndDate { get; init; }
+
+  private const int MIN_DISCOUNT_PERCENTAGE = 0;
+  private const int MAX_DISCOUNT_PERCENTAGE = 100;
 
   private Promotion()
     : base(Guid.NewGuid()) { }
@@ -19,15 +24,42 @@ public sealed class Promotion : Entity<Guid>
     EndDate = endDate;
   }
 
-  public static Promotion Create(int discountPercentage, DateTime startDate, DateTime endDate)
+  public static Result<Promotion> Create(
+    int discountPercentage,
+    DateTime startDate,
+    DateTime endDate
+  )
   {
-    if (discountPercentage < 0 || discountPercentage > 100)
+    if (
+      discountPercentage < MIN_DISCOUNT_PERCENTAGE
+      || discountPercentage > MAX_DISCOUNT_PERCENTAGE
+    )
     {
-      throw new ArgumentException("Discount percentage must be between 0 and 100");
+      return OutOfBoundsError<int>.GetResult(
+        nameof(DiscountPercentage),
+        "Percentage is out of bounds.",
+        MIN_DISCOUNT_PERCENTAGE,
+        MAX_DISCOUNT_PERCENTAGE
+      );
     }
 
-    return new(Guid.NewGuid(), discountPercentage, startDate, endDate);
+    if (startDate >= endDate)
+    {
+      return InvalidDateRangeError.GetResult(
+        nameof(startDate),
+        "Range Invalid",
+        startDate,
+        endDate
+      );
+    }
+
+    var promotion = new Promotion(Guid.NewGuid(), discountPercentage, startDate, endDate);
+
+    return Result.Ok(promotion);
   }
+
+  public static Promotion CreateEmpty() =>
+    new(Guid.NewGuid(), 0, DateTime.MinValue, DateTime.MaxValue);
 
   public override IEnumerable<object> GetEqualityComponents()
   {
