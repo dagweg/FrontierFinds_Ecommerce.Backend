9abe93481b0d6a87e6fa64b4fa385a9df0b7cbd8
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:03:10 +0300

    feat: `cart item` result obj

---
+++ Ecommerce.Application/UseCases/Products/Common/CartItemResult.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Application.UseCases.Products.Common;
+
+public class CartItemResult
+{
+  public required string Id { get; set; }
+  public required int Quantity { get; set; }
+  public required ProductResult Product { get; set; }
+}
