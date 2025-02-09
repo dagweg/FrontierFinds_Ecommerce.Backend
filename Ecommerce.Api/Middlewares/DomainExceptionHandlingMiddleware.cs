43d08f801ea75b65ceba87cb9cff337974c250b3
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:42:13 +0300

    use exception handler factory w/ strategy

--- Ecommerce.Api/Middlewares/DomainExceptionHandlingMiddleware.cs
+++ Ecommerce.Api/Middlewares/DomainExceptionHandlingMiddleware.cs
@@ -1,46 +1,46 @@
-using System.Text.Json;
-using Ecommerce.Domain.Common.Exceptions;
-using Microsoft.AspNetCore.Mvc;
+// using System.Text.Json;
+//
+// using Microsoft.AspNetCore.Mvc;
 
-namespace Ecommerce.Api.Middlewares;
+// namespace Ecommerce.Api.Middlewares;
 
-public class DomainExceptionHandlingMiddleware
-{
-  private readonly RequestDelegate _next;
-  private readonly ILogger<DomainExceptionHandlingMiddleware> _logger;
+// public class DomainExceptionHandlingMiddleware
+// {
+//   private readonly RequestDelegate _next;
+//   private readonly ILogger<DomainExceptionHandlingMiddleware> _logger;
 
-  public DomainExceptionHandlingMiddleware(
-    RequestDelegate next,
-    ILogger<DomainExceptionHandlingMiddleware> logger
-  )
-  {
-    _next = next;
-    _logger = logger;
-  }
+//   public DomainExceptionHandlingMiddleware(
+//     RequestDelegate next,
+//     ILogger<DomainExceptionHandlingMiddleware> logger
+//   )
+//   {
+//     _next = next;
+//     _logger = logger;
+//   }
 
-  public async Task Invoke(HttpContext context)
-  {
-    try
-    {
-      await _next(context);
-    }
-    catch (DomainException ex)
-    {
-      _logger.LogError(ex, "Domain exception occurred: {ExceptionType}", ex.GetType().Name);
+//   public async Task Invoke(HttpContext context)
+//   {
+//     try
+//     {
+//       await _next(context);
+//     }
+//     catch (FluentErrorBase ex)
+//     {
+//       _logger.LogError(ex, "Domain exception occurred: {ExceptionType}", ex.GetType().Name);
 
-      var exception = ex.GetType();
-      var problemDetails = new ProblemDetails
-      {
-        Type = exception.Name.Replace("Exception", "Failure"),
-        Title = exception.Name.Replace("Exception", " Error"),
-        Detail = ex.Message,
-        Status = StatusCodes.Status400BadRequest,
-      };
+//       var exception = ex.GetType();
+//       var problemDetails = new ProblemDetails
+//       {
+//         Type = exception.Name.Replace("Exception", "Failure"),
+//         Title = exception.Name.Replace("Exception", " Error"),
+//         Detail = ex.Message,
+//         Status = StatusCodes.Status400BadRequest,
+//       };
 
-      context.Response.StatusCode = problemDetails.Status.Value;
-      context.Response.ContentType = "application/problem+json";
+//       context.Response.StatusCode = problemDetails.Status.Value;
+//       context.Response.ContentType = "application/problem+json";
 
-      await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
-    }
-  }
-}
+//       await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails));
+//     }
+//   }
+// }
--- Ecommerce.Api/Middlewares/ExceptionHandlingMiddleware.cs
+++ Ecommerce.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -1,8 +1,10 @@
+using System.Reflection.Metadata;
+using Ecommerce.Api.Exceptions;
 using Ecommerce.Application.Common.Utilities;
-using Ecommerce.Domain.Common.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
 
 namespace Ecommerce.Api.Middlewares;
 
@@ -10,14 +12,17 @@ public class ExceptionHandlingMiddleware
 {
   private readonly RequestDelegate _next;
   private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+  private readonly ExceptionHandlerFactory _exceptionHandlerFactory;
 
   public ExceptionHandlingMiddleware(
     RequestDelegate next,
-    ILogger<ExceptionHandlingMiddleware> logger
+    ILogger<ExceptionHandlingMiddleware> logger,
+    ExceptionHandlerFactory exceptionHandlerFactory
   )
   {
     _next = next;
     _logger = logger;
+    _exceptionHandlerFactory = exceptionHandlerFactory;
   }
 
   public async Task InvokeAsync(HttpContext context)
@@ -28,21 +33,9 @@ public class ExceptionHandlingMiddleware
     }
     catch (Exception ex)
     {
-      var problemDetails = new ProblemDetails
-      {
-        Status = StatusCodes.Status500InternalServerError,
-        Type = "InternalServerFaliure",
-        Title = "Internal Server Error",
-        Detail = "An internal server error has occured. Please try again later or contact support.",
-        Extensions = { { "traceId", context.TraceIdentifier } },
-      };
-
-      context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-      context.Response.Headers.Add("X-Trace-ID", context.TraceIdentifier);
-
-      await context.Response.WriteAsJsonAsync(problemDetails);
-
-      _logger.LogFormattedError(ex, context.TraceIdentifier);
+      await _exceptionHandlerFactory
+        .GetHandler(ex)
+        .TryHandleAsync(context, ex, CancellationToken.None);
     }
   }
 }
