fee2ea1a63d8775c5607e4e5b76be2ab3af19881
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:09:06 +0300

    feat: inject 'email settings' to use 'IOptions<EmailSettings>' and also 'Product validation service'

--- Ecommerce.Application/DependencyInjection.cs
+++ Ecommerce.Application/DependencyInjection.cs
@@ -5,14 +5,22 @@ using Ecommerce.Application.Common;
 using Ecommerce.Application.Common.Interfaces.Validation;
 using Ecommerce.Application.Services.Validation;
 using Ecommerce.Application.UseCases.Images.Commands;
+using Ecommerce.Application.UseCases.Smtp.Commands.SendEmail;
 using FluentValidation;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
-  public static IServiceCollection AddApplication(this IServiceCollection services)
+  public static IServiceCollection AddApplication(
+    this IServiceCollection services,
+    IConfiguration configuration
+  )
   {
+    // Load EmailSettings Configuration from appsettings.json
+    services.Configure<EmailSettings>(configuration.GetSection(EmailSettings.SectionName));
+
     // MediatR Registration
     services.AddMediatR(ApplicationAssembly.Assembly);
 
@@ -25,6 +33,7 @@ public static class DependencyInjection
     services.AddTransient<IValidator<CreateImageCommand>, CreateImageCommandValidator>();
 
     services.AddScoped<IUserValidationService, UserValidationService>();
+    services.AddScoped<IProductValidationService, ProductValidationService>();
 
     return services;
   }
