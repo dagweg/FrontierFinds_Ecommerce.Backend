cfde8b96ec6dbb65bb3db54c2c17ddbebe984c95
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 13:44:20 +0300

    feat: cart contract

---
+++ Ecommerce.Contracts/Cart/CartItemResponse.cs
@@ -0,0 +1,12 @@
+namespace Ecommerce.Contracts.Cart;
+
+public class CartItemResponse
+{
+  public required string Id { get; set; }
+  public required string ProductId { get; set; }
+  public required string ProductName { get; set; }
+  public required string ProductDescription { get; set; }
+  public required string ProductImage { get; set; }
+  public decimal ProductPrice { get; set; }
+  public int Quantity { get; set; }
+}
---
+++ Ecommerce.Contracts/Cart/CartResponse.cs
@@ -0,0 +1,14 @@
+namespace Ecommerce.Contracts.Cart;
+
+public class CartResponse
+{
+  public int TotalCount { get; set; }
+  public decimal TotalPrice { get; set; }
+  public List<CartItemResponse> Items { get; set; }
+
+  public CartResponse(List<CartItemResponse> items, decimal totalPrice)
+  {
+    Items = items;
+    TotalPrice = totalPrice;
+  }
+};
