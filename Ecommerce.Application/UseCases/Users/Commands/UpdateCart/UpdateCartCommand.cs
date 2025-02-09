236262411f06633bc3f004fd913f0a648070de18
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:06:55 +0300

    feat: 'update cart' use case

---
+++ Ecommerce.Application/UseCases/Users/Commands/UpdateCart/UpdateCartCommand.cs
@@ -0,0 +1,9 @@
+using FluentResults;
+using MediatR;
+
+namespace Ecommerce.Application.UseCases.Users.Commands.UpdateCart;
+
+public class UpdateCartCommand : IRequest<Result>
+{
+  public List<UpdateCartItemCommand> CartItems { get; set; } = [];
+}
