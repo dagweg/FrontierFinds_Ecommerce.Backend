20a9434947887bded2eb60a4ac0b31b6fc2e0819
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:05:28 +0300

    feat: `change password` use case

---
+++ Ecommerce.Application/UseCases/Users/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -0,0 +1,10 @@
+using FluentResults;
+using MediatR;
+
+namespace Ecommerce.Application.UseCases.Users.Commands.ChangePassword;
+
+public class ChangePasswordCommand : IRequest<Result>
+{
+  public required string CurrentPassword { get; set; }
+  public required string NewPassword { get; set; }
+}
