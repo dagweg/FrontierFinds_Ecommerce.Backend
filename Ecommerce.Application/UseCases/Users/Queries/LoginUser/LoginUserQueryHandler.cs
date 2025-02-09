ce8506e2099931db646bfee3d3d6f8c3a7cf40fe
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:08:30 +0300

    feat: check for 'hashed password' when logging user

--- Ecommerce.Application/UseCases/Users/Queries/LoginUser/LoginUserQueryHandler.cs
+++ Ecommerce.Application/UseCases/Users/Queries/LoginUser/LoginUserQueryHandler.cs
@@ -6,8 +6,10 @@ using Ecommerce.Application.Common.Interfaces.Persistence;
 using Ecommerce.Application.Common.Interfaces.Providers.Localization;
 using Ecommerce.Application.UseCases.Users.Common;
 using Ecommerce.Domain.Common.ValueObjects;
+using Ecommerce.Domain.UserAggregate;
 using FluentResults;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -20,13 +22,15 @@ public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, Result<Auth
   private readonly IJwtTokenGenerator _jwtTokenGenerator;
   private readonly IMapper _mapper;
   private readonly IAuthenticationMessages _authValidationMessages;
+  private readonly IPasswordHasher<User> _passwordHasher;
 
   public LoginUserQueryHandler(
     IUserRepository userRespository,
     IJwtTokenGenerator jwtTokenGenerator,
     ILogger<LoginUserQueryHandler> logger,
     IMapper mapper,
-    IAuthenticationMessages authValidationMessages
+    IAuthenticationMessages authValidationMessages,
+    IPasswordHasher<User> passwordHasher
   )
   {
     _userRepository = userRespository;
@@ -34,6 +38,7 @@ public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, Result<Auth
     _jwtTokenGenerator = jwtTokenGenerator;
     _mapper = mapper;
     _authValidationMessages = authValidationMessages;
+    _passwordHasher = passwordHasher;
   }
 
   public async Task<Result<AuthenticationResult>> Handle(
@@ -41,56 +46,43 @@ public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, Result<Auth
     CancellationToken cancellationToken
   )
   {
-    try
-    {
-      // 1. Check if the user with the given email exists
-      var user = await _userRepository.GetByEmailAsync(Email.Create(query.Email));
+    // 1. Check if the user with the given email exists
+    var user = await _userRepository.GetByEmailAsync(Email.Create(query.Email));
 
-      if (user is null)
-        return Result.Fail(
-          new AuthenticationError(
-            nameof(query.Email),
-            _authValidationMessages.EmailOrPasswordIncorrect
-          )
-        );
-
-      // 2. Check if the password is correct
-      // Note: use bcrypt hash for comparing the password in production,
-      // it's okay to compare literal passwords in dev envrionment
-      if (!user.Password.Value.Equals(query.Password))
-      {
-        return Result.Fail(
-          new AuthenticationError(
-            nameof(query.Password),
-            _authValidationMessages.EmailOrPasswordIncorrect
-          )
-        );
-      }
-
-      // 3. Generate a JWT token
-      var token = _jwtTokenGenerator.GenerateToken(
-        user.Id,
-        user.Email,
-        user.FirstName,
-        user.LastName
+    if (user is null)
+      return Result.Fail(
+        new AuthenticationError(nameof(Email), _authValidationMessages.UserNotFound)
       );
 
-      // 4. Return the user's information and the token
-      var result = _mapper.Map<AuthenticationResult>(user);
-      result.Token = token;
-
-      return Result.Ok(result);
-    }
-    catch (Exception ex)
+    // 2. Check if the password is correct
+    if (
+      _passwordHasher.VerifyHashedPassword(user, user.Password.ValueHash, query.Password)
+      == PasswordVerificationResult.Failed
+    )
     {
-      _logger.LogError(
-        "An error occurred while logging in the User with Email: {@Email}. Exception: {@ExceptionMessage} {@StackTrace}",
-        query.Email,
-        ex.Message,
-        ex.StackTrace
+      return Result.Fail(
+        new AuthenticationError(
+          nameof(query.Password),
+          _authValidationMessages.EmailOrPasswordIncorrect
+        )
       );
-
-      return Result.Fail("An error occurred while logging in the user");
     }
+
+    // 3. Generate a JWT token
+    var tokenResult = _jwtTokenGenerator.GenerateToken(
+      user.Id,
+      user.Email,
+      user.FirstName,
+      user.LastName
+    );
+
+    if (tokenResult.IsFailed)
+      return tokenResult.ToResult();
+
+    // 4. Return the user's information and the token
+    var result = _mapper.Map<AuthenticationResult>(user);
+    result.Token = tokenResult.Value;
+
+    return Result.Ok(result);
   }
 }
