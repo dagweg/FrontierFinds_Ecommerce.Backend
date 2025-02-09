24be119ec8198ae53afee612e9ac116327263cba
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:00:29 +0300

    `GetUserId` method

--- Ecommerce.Application/Common/Interfaces/Providers/Context/IUserContextService.cs
+++ Ecommerce.Application/Common/Interfaces/Providers/Context/IUserContextService.cs
@@ -1,8 +1,10 @@
+using FluentResults;
+
 namespace Ecommerce.Application.Common.Interfaces.Providers.Context;
 
 // This interface is used to get the current user's ID
 // This is useful when you want to know who is the current user logged in
 public interface IUserContextService
 {
-  string? GetUserId();
+  Result<string> GetUserId();
 }
