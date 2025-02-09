6f065ab9f81ef606346a853d2d618af5f54ad23a
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-09 14:01:19 +0300

    feat: generic result object for queries of entities

---
+++ Ecommerce.Application/Common/Models/GetAllResult.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Application.Common.Models;
+
+public class GetAllResult<TObject>
+{
+  public IEnumerable<TObject> Items { get; set; } = [];
+  public required int TotalItems { get; set; }
+  public required int TotalItemsFetched { get; set; }
+}
