13faa09a7094538042ac7298ca0353f0fe977f69
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-14 02:22:40 +0300

    refactor: simplify Stock value object by removing reserved property and related logic

--- Ecommerce.Domain/ProductAggregate/ValueObjects/Stock.cs
+++ Ecommerce.Domain/ProductAggregate/ValueObjects/Stock.cs
@@ -7,37 +7,25 @@ using FluentResults;
 public sealed class Stock : ValueObject
 {
   public Quantity Quantity { get; set; } = Quantity.Empty;
-  public int Reserved { get; set; } = 0;
 
-  public static Stock Empty => new(Quantity.Empty, 0);
+  public static Stock Empty => new(Quantity.Empty);
 
   private Stock() { }
 
-  private Stock(Quantity quantity, int reserved)
+  private Stock(Quantity quantity)
   {
     Quantity = quantity;
-    Reserved = reserved;
   }
 
-  public static Stock Create(Quantity quantity, int reserved = 0)
+  public static Stock Create(Quantity quantity)
   {
-    return new(quantity, reserved);
+    return new(quantity);
   }
 
   public void UpdateQuantity(Quantity quantity) => Quantity = quantity;
 
-  public Result<Stock> UpdateReserved(int reserved)
-  {
-    if (reserved < 0)
-      return BelowZeroError.GetResult(nameof(reserved), "Cannot be less than 0.");
-    Reserved = reserved;
-
-    return Result.Ok(this);
-  }
-
   public override IEnumerable<object> GetEqualityComponents()
   {
     yield return Quantity;
-    yield return Reserved;
   }
 }
