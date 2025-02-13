f276a0e7147588e7bc47276821a87a440b28cf00
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-14 02:21:44 +0300

    feat: update CreateImageRequest to use IFormFile for image uploads

--- Ecommerce.Contracts/Image/CreateImageRequest.cs
+++ Ecommerce.Contracts/Image/CreateImageRequest.cs
@@ -1,14 +1,13 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace Ecommerce.Contracts.Image;
 
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 public class CreateImageRequest
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
   [Required]
-  public string Base64 { get; set; }
-  public string FileType { get; set; }
+  public IFormFile ImageFile { get; set; }
   public string? FileName { get; set; } = null;
-  public long? FileSize { get; set; } = null;
-}
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+}
