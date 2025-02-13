b14babc1d29dd026156fb18599b36be6799f175f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-14 02:20:16 +0300

    feat: update ImageResult class to require Url and add ObjectIdentifier property

--- Ecommerce.Application/UseCases/Images/Common/ImageResult.cs
+++ Ecommerce.Application/UseCases/Images/Common/ImageResult.cs
@@ -2,19 +2,6 @@ namespace Ecommerce.Application.UseCases.Images.Common;
 
 public class ImageResult
 {
-  public string Url { get; set; }
-  public string FileType { get; set; }
-  public string? FileName { get; set; }
-  public string? FileSize { get; set; }
-
-  public ImageResult(string url, string fileType, string? fileName = null, string? fileSize = null)
-  {
-    Url = url;
-    FileType = fileType;
-    FileName = fileName;
-    FileSize = fileSize;
-  }
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
-  public ImageResult() { }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+  public required string Url { get; set; }
+  public required string ObjectIdentifier { get; set; }
 }
