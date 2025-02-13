4446641bfc3164251f9880be8a1cb642c81cbf5b
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-14 02:16:38 +0300

    feat: implement product image strategies for various views

---
+++ Ecommerce.Application/Behaviors/Strategies/ProductImageStrategies/BottomImageStrategy.cs
@@ -0,0 +1,12 @@
+using Ecommerce.Application.Common.Interfaces.Strategies;
+using Ecommerce.Domain.ProductAggregate.Entities;
+
+namespace Ecommerce.Application.Behaviors.Strategies.ProductImageStrategies;
+
+public class BottomImageStrategy : IProductImageStrategy
+{
+  public void Apply(ProductImages productImages, ProductImage productImage)
+  {
+    productImages.WithBottomImage(productImage);
+  }
+}
