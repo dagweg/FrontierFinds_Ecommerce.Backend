e118d8aabc555b1661f9a51080e1ec6d9a312801
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-14 02:18:47 +0300

    refactor: simplify ImageMappingProfile by removing unused mapping configuration

--- Ecommerce.Application/Common/Mapping/ImageMappingProfile.cs
+++ Ecommerce.Application/Common/Mapping/ImageMappingProfile.cs
@@ -1,13 +1,10 @@
 using AutoMapper;
-using Ecommerce.Application.UseCases.Images.Commands;
 using Ecommerce.Application.UseCases.Images.Common;
+using Ecommerce.Application.UseCases.Images.CreateImage;
 
 namespace Ecommerce.Application.Common.Mapping;
 
 public class ImageMappingProfile : Profile
 {
-  public ImageMappingProfile()
-  {
-    CreateMap<CreateImageCommand, ImageResult>();
-  }
+  public ImageMappingProfile() { }
 }
