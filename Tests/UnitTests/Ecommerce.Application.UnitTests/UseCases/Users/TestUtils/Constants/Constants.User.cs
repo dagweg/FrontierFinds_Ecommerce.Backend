00bb66a40e1e3b97dbd9aa8ea4096528af93a220
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-02-17 01:14:03 +0300

    feat: add user creation utility method and unique user ID constant for testing

--- Tests/UnitTests/Ecommerce.Application.UnitTests/UseCases/Users/TestUtils/Constants/Constants.User.cs
+++ Tests/UnitTests/Ecommerce.Application.UnitTests/UseCases/Users/TestUtils/Constants/Constants.User.cs
@@ -1,9 +1,12 @@
+using Ecommerce.Domain.UserAggregate.ValueObjects;
+
 namespace Ecommerce.Application.UnitTests.UseCases.Users.TestUtils.Constants;
 
 public partial class Constants
 {
   public record User
   {
+    public static readonly string Id = Guid.NewGuid().ToString();
     public const string FirstName = "John";
     public const string LastName = "Doe";
     public const string Email = "johndoe@gmail.com";
@@ -11,5 +14,19 @@ public partial class Constants
     public const string ConfirmPassword = "abcd1234!@#$ABCD";
     public const string PhoneNumber = "0911223344";
     public const string CountryCode = "251";
+
+    public static Domain.UserAggregate.User Create()
+    {
+      return Domain.UserAggregate.User.Create(
+        Name.Create(Constants.User.FirstName),
+        Name.Create(Constants.User.LastName),
+        Domain.Common.ValueObjects.Email.Create(Constants.User.Email),
+        Domain
+          .Common.ValueObjects.Password.Create(Constants.User.Password, Constants.User.Password)
+          .Value,
+        Domain.Common.ValueObjects.PhoneNumber.Create(Constants.User.PhoneNumber),
+        Constants.User.CountryCode
+      );
+    }
   }
 }
