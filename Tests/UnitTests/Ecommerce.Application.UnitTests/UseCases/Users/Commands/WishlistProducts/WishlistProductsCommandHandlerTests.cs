532c8cc0a44f8248375a4f46a1cc4f90cba83b4f
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-06 13:25:45 +0300

    test: add unit tests for WishlistProductsCommandHandler to verify wishlist addition and validation

---
+++ Tests/UnitTests/Ecommerce.Application.UnitTests/UseCases/Users/Commands/WishlistProducts/WishlistProductsCommandHandlerTests.cs
@@ -0,0 +1,70 @@
+using System.Threading.Tasks;
+using Ecommerce.Application.Common.Interfaces.Persistence;
+using Ecommerce.Application.Common.Interfaces.Providers.Context;
+using Ecommerce.Application.UseCases.Users.Commands.WishlistProducts;
+using Ecommerce.Domain.ProductAggregate.ValueObjects;
+using Ecommerce.Domain.UserAggregate.ValueObjects;
+using FluentAssertions;
+using FluentResults;
+using Moq;
+
+namespace Ecommerce.UnitTests.Ecommerce.Application.UnitTests.UseCases.Users.Commands.WishlistProducts;
+
+public class WishlistProductsCommandHandlerTests
+{
+  private readonly WishlistProductsCommandHandler _wishlistProductsCommandHandler;
+  private readonly Mock<IUserRepository> _userRepositoryMock;
+  private readonly Mock<IUserContextService> _userContextServiceMock;
+  private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+  private readonly Mock<IProductRepository> _productRepositoryMock;
+
+  public WishlistProductsCommandHandlerTests()
+  {
+    _userRepositoryMock = new Mock<IUserRepository>();
+    _userContextServiceMock = new Mock<IUserContextService>();
+    _unitOfWorkMock = new Mock<IUnitOfWork>();
+    _productRepositoryMock = new Mock<IProductRepository>();
+
+    _userContextServiceMock.Setup(x => x.GetValidUserId()).Returns(Result.Ok());
+
+    _wishlistProductsCommandHandler = new WishlistProductsCommandHandler(
+      userRepository: _userRepositoryMock.Object,
+      userContextService: _userContextServiceMock.Object,
+      unitOfWork: _unitOfWorkMock.Object,
+      productRepository: _productRepositoryMock.Object
+    );
+  }
+
+  [Fact]
+  public async Task HandlerShould_ReturnSuccess_WhenWishlistsAreAdded()
+  {
+    // Given
+    var command = Utils.Wishlist.CreateWishlistProductsCommand();
+
+    _userRepositoryMock
+      .Setup(x => x.AddToWishlistRangeAsync(It.IsAny<UserId>(), It.IsAny<List<ProductId>>()))
+      .ReturnsAsync(true);
+
+    // When
+    var result = await _wishlistProductsCommandHandler.Handle(command, default);
+
+    // Then
+    result.IsSuccess.Should().BeTrue();
+  }
+
+  [Fact]
+  public async Task HandlerShould_ReturnFailure_WhenWishlistProductIdIsInvalid()
+  {
+    // Given
+    var command = Utils.Wishlist.CreateWishlistProductsCommand() with
+    {
+      ProductIds = new List<string> { "invalid-id" },
+    };
+
+    // When
+    var result = await _wishlistProductsCommandHandler.Handle(command, default);
+
+    // Then
+    result.IsFailed.Should().BeTrue();
+  }
+}
