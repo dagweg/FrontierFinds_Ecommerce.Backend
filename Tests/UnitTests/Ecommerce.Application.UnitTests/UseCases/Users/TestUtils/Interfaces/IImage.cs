0f2729fbbadc18e723e04357869d90d3e7743e56
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-06 13:26:48 +0300

    refactor: remove unused IImage interface from unit tests

--- Tests/UnitTests/Ecommerce.Application.UnitTests/UseCases/Users/TestUtils/Interfaces/IImage.cs
+++
@@ -1,8 +0,0 @@
-namespace Ecommerce.Application.UnitTests.UseCases.Users.TestUtils.Interfaces;
-
-public interface IImage
-{
-  static string FileName { get; set; } = null!;
-  static string ImageData { get; set; } = null!;
-  static Stream ImageStream { get; set; } = null!;
-}
