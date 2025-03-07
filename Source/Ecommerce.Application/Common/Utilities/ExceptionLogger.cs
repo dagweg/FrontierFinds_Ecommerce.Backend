c9ce60b032c18c996bdd21623d6651f5aab88668
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-07 07:06:44 +0300

    fix: enhance ExceptionLogger to include optional status code in error logging

--- Source/Ecommerce.Application/Common/Utilities/ExceptionLogger.cs
+++ Source/Ecommerce.Application/Common/Utilities/ExceptionLogger.cs
@@ -4,11 +4,17 @@ namespace Ecommerce.Application.Common.Utilities;
 
 public static class ExceptionLogger
 {
-  public static void LogFormattedError(this ILogger logger, Exception exception, string traceId)
+  public static void LogFormattedError(
+    this ILogger logger,
+    Exception exception,
+    string traceId,
+    int? statusCode = null
+  )
   {
     logger.LogError(
       $"{Environment.NewLine}Exception: {exception.GetType()}"
         + $"{Environment.NewLine}Message: {exception.Message}"
+        + $"{Environment.NewLine}StatusCode: {statusCode}"
         + $"{Environment.NewLine}TraceId: {traceId}"
         + $"{Environment.NewLine}StackTrace: {exception.StackTrace}"
     );
