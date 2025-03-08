3e6fc83d04cfbe7f9268268eb74a6ca6dd5f3460
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-08 09:21:39 +0300

    feat: add login test to AuthenticationControllerTests for successful login scenario

--- Tests/IntegrationTests/Api/AuthenticationControllerTests.cs
+++ Tests/IntegrationTests/Api/AuthenticationControllerTests.cs
@@ -3,23 +3,24 @@ using System.Net;
 using System.Net.Http.Json;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Ecommerce.Application.UseCases.Users.Common;
 using Ecommerce.Contracts.Authentication;
+using Ecommerce.Infrastructure.Persistence.EfCore;
 using Ecommerce.Tests.Shared;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit.Sdk;
 
 namespace Ecommerce.IntegrationTests.Api;
 
-public class AuthenticationControllerTests : IClassFixture<EcommerceWebApplicationFactoryFixture>
+public class AuthenticationControllerTests : IntegrationTestBase
 {
-  private readonly HttpClient _httpClient;
-
   public AuthenticationControllerTests(EcommerceWebApplicationFactoryFixture factoryFixture)
-  {
-    _httpClient = factoryFixture.CreateClient();
-  }
+    : base(factoryFixture) { }
 
   [Fact]
   public async Task RegisterShould_ReturnCreated_WhenRegistrationIsSuccessful()
@@ -32,5 +33,30 @@ public class AuthenticationControllerTests : IClassFixture<EcommerceWebApplicati
 
     // Assert
     response.StatusCode.Should().Be(HttpStatusCode.Created);
+  }
+
+  [Fact]
+  public async Task LoginShould_ReturnToken_WhenLoginIsSuccessful()
+  {
+    // arrange
+    var registerRequest = Utils.User.CreateRegisterRequest();
+    var loginRequest = Utils.User.CreateLoginRequest();
+
+    // act
+    await _httpClient.PostAsJsonAsync("auth/register", registerRequest);
+    var response = await _httpClient.PostAsJsonAsync("auth/login", loginRequest);
+    var content = await response.Content.ReadFromJsonAsync<AuthenticationResult>();
+
+    System.Console.WriteLine(
+      JsonSerializer.Serialize(response, new JsonSerializerOptions { WriteIndented = true })
+    );
+    System.Console.WriteLine(
+      JsonSerializer.Serialize(content, new JsonSerializerOptions { WriteIndented = true })
+    );
+
+    // assert
+    response.StatusCode.Should().Be(HttpStatusCode.OK);
+    content.Should().NotBeNull();
+    content.Token.Should().NotBeNull();
   }
 }
