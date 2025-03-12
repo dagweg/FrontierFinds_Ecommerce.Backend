b49c5cc92de4900319f0cd996e1252c119679e55
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-12 23:27:48 +0300

    feat: integrate Stripe payment service into order creation process and update total price calculation

--- Source/Ecommerce.Application/UseCases/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
+++ Source/Ecommerce.Application/UseCases/Orders/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -1,10 +1,12 @@
 using AutoMapper;
 using Ecommerce.Application.Common.Interfaces.Persistence;
+using Ecommerce.Application.Common.Interfaces.Providers.Payment.Stripe;
 using Ecommerce.Application.Common.Utilities;
 using Ecommerce.Domain.Common.ValueObjects;
 using Ecommerce.Domain.OrderAggregate;
 using Ecommerce.Domain.OrderAggregate.Entities;
 using Ecommerce.Domain.OrderAggregate.ValueObjects;
+using Ecommerce.Domain.PaymentAggregate.Enums;
 using Ecommerce.Domain.ProductAggregate.ValueObjects;
 using Ecommerce.Domain.UserAggregate.ValueObjects;
 using FluentResults;
@@ -17,7 +19,8 @@ public class CreateOrderCommandHandler(
   IOrderRepository orderRepository,
   IProductRepository productRepository,
   IUnitOfWork unitOfWork,
-  IMapper mapper
+  IMapper mapper,
+  IStripeService stripeService
 ) : IRequestHandler<CreateOrderCommand, Result>
 {
   public async Task<Result> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
@@ -49,19 +52,19 @@ public class CreateOrderCommandHandler(
       orderProductKeysWithQuantity.Select(kvp => kvp.Key)
     );
 
-    var totalPrice = 0m;
+    long totalAmountInCents = 0L;
 
     List<OrderItem> orderItems = productsFromDb
       .Select(p =>
       {
-        totalPrice += p.Value.Price.Value; // accumulate totalPrice (OrderTotal)
+        totalAmountInCents += p.Value.Price.ValueInCents; // accumulate totalPrice (OrderTotal)
 
         // p.Key  and the 2nd arg is the productId:quantity  w/c we precomputed previously
         return OrderItem.Create(p.Key, orderProductKeysWithQuantity[p.Key], p.Value.Price);
       })
       .ToList();
 
-    OrderTotal orderTotal = OrderTotal.Create(totalPrice, currency: Price.BASE_CURRENCY);
+    OrderTotal orderTotal = OrderTotal.Create(totalAmountInCents, currency: Price.BASE_CURRENCY);
 
     ShippingAddress shippingAddress = mapper.Map<ShippingAddress>(request.ShippingAddress);
 
@@ -73,6 +76,20 @@ public class CreateOrderCommandHandler(
     // var paymentResult = await paymentService.Process(...);
 
 
+    var result = await stripeService.CreateCheckoutSessionAsync(
+      new CheckoutSessionRequest
+      {
+        lineItems = orderItems
+          .Select(i => new OrderItemCheckout(
+            productsFromDb[i.ProductId].Name,
+            i.Quantity,
+            i.Price.ValueInCents
+          ))
+          .ToList(),
+        paymentMethod = PaymentMethod.CreditCard,
+        paymentMode = PaymentMode.Payment, // single payment (not subscription)
+      }
+    );
 
     await orderRepository.AddAsync(order);
 
