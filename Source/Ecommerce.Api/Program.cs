56e056366668bd714245b4c5a7823b3293fe66aa
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 21:19:58 +0300

    feat: implement database seeding for users, products, and categories

--- Source/Ecommerce.Api/Program.cs
+++ Source/Ecommerce.Api/Program.cs
@@ -5,6 +5,8 @@
 namespace Ecommerce.Api;
 
 using System.Configuration;
+using System.Data;
+using System.Threading.Tasks;
 using Ecommerce.Api;
 using Ecommerce.Api.Mapping;
 using Ecommerce.Api.Middlewares;
@@ -12,13 +14,19 @@ using Ecommerce.Application;
 using Ecommerce.Application.UseCases.Images.Common;
 using Ecommerce.Application.UseCases.Images.CreateImage;
 using Ecommerce.Contracts.Image;
+using Ecommerce.Domain.Common.Entities;
+using Ecommerce.Domain.ProductAggregate;
+using Ecommerce.Domain.UserAggregate;
 using Ecommerce.Infrastructure;
 using Ecommerce.Infrastructure.Common;
+using Ecommerce.Infrastructure.Persistence.EfCore;
 using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 
 public class Program
 {
-  public static void Main(string[] args)
+  public static async Task Main(string[] args)
   {
     try
     {
@@ -67,12 +75,151 @@ public class Program
 
         app.MapControllers();
 
+        // await SeedDatabase(app);
+
         app.Run();
       }
     }
     catch (Exception ex)
     {
       Console.WriteLine($"Server terminated unexpectedly. {ex}");
+    }
+  }
+
+  public static async Task SeedDatabase(WebApplication app)
+  {
+    using var scope = app.Services.CreateScope();
+    {
+      var services = scope.ServiceProvider;
+      var db = services.GetRequiredService<EfCoreContext>();
+
+      try
+      {
+        await SeedCategories(db);
+
+        if (app.Environment.IsDevelopment())
+        {
+          await SeedUsers(db);
+          await SeedProducts(db);
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"An error occurred while seeding the database.");
+        System.Console.WriteLine(ex);
+        throw;
+      }
+    }
+  }
+
+  public static async Task SeedUsers(EfCoreContext db)
+  {
+    Console.WriteLine("Seeding Users");
+    var seed = Seeding.User.GetSeed();
+
+    List<User> users = new();
+    foreach (var user in seed)
+    {
+      // make sure no duplicate users are added
+      var found = await db.Users.FindAsync(user.Id);
+
+      if (found == null) // new
+      {
+        users.Add(user);
+      }
+      else
+      {
+        // update existing user
+        db.Entry(found).CurrentValues.SetValues(user);
+      }
+    }
+
+    await db.Users.AddRangeAsync(users);
+    int i = await db.SaveChangesAsync();
+    Console.WriteLine($"User seeding finished. Changes {i}");
+  }
+
+  public static async Task SeedProducts(EfCoreContext db)
+  {
+    Console.WriteLine("Seeding Products");
+    var seed = Seeding.Product.GetSeed();
+
+    List<Product> products = new();
+    foreach (var prod in seed)
+    {
+      // make sure no duplicate products are added
+      var found = await db.Products.AsNoTracking().FirstOrDefaultAsync(p => p.Id == prod.Id);
+
+      if (found == null) // new
+      {
+        products.Add(prod);
+      }
+      else
+      {
+        // update existing product
+        // db.Entry(found).CurrentValues.SetValues(prod);
+        db.Products.Attach(prod);
+        db.Entry(prod).State = EntityState.Modified;
+      }
+    }
+
+    await db.Products.AddRangeAsync(products);
+    int i = await db.SaveChangesAsync();
+    Console.WriteLine($"Product seeding finished. Changes: {i}");
+  }
+
+  public static async Task SeedCategories(EfCoreContext db)
+  {
+    Console.WriteLine("Seeding Categories");
+    var seed = Seeding.Categories.GetSeed();
+
+    try
+    {
+      var uow = new UnitOfWork(db, new Logger<UnitOfWork>(new LoggerFactory()));
+
+      // Ensure IDENTITY_INSERT is set within the same transaction as SaveChanges
+      await uow.ExecuteTransactionAsync(async () =>
+      {
+        // Enable IDENTITY_INSERT
+        await db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT Categories ON;");
+
+        List<Category> categories = new();
+        foreach (var category in seed)
+        {
+          var foundEntity = await db
+            .Categories.AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Id == category.Id);
+          if (foundEntity != null)
+          {
+            // Update existing entity
+            // db.Entry(foundEntity).CurrentValues.SetValues(category);
+            db.Categories.Attach(category);
+            db.Entry(category).State = EntityState.Modified;
+          }
+          else
+          {
+            // Insert new category
+            categories.Add(category);
+          }
+        }
+
+        if (categories.Any())
+        {
+          await db.Categories.AddRangeAsync(categories);
+        }
+
+        return 1;
+      });
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine($"Error seeding categories: {ex.Message}");
+      throw; // Re-throw to handle upstream
+    }
+    finally
+    {
+      // Ensure IDENTITY_INSERT is turned off
+      await db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT Categories OFF;");
     }
   }
 }
