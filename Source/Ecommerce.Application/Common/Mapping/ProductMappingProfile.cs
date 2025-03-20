b119b3ef3ed6e06cbdd20be80a851e0d6ef16da7
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 21:20:49 +0300

    feat: enhance mapping profiles to include Category and ProductReview mappings

--- Source/Ecommerce.Application/Common/Mapping/ProductMappingProfile.cs
+++ Source/Ecommerce.Application/Common/Mapping/ProductMappingProfile.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
+using Ecommerce.Application.Common.Models;
 using Ecommerce.Application.UseCases.Images.Common;
 using Ecommerce.Application.UseCases.Products.Common;
+using Ecommerce.Domain.Common.Entities;
 using Ecommerce.Domain.ProductAggregate;
 using Ecommerce.Domain.ProductAggregate.Entities;
 
@@ -24,6 +26,10 @@ public class ProductMappingProfile : Profile
 
     CreateMap<ProductTag, TagResult>();
 
+    CreateMap<Category, CategoryResult>();
+
+    CreateMap<ProductReview, ProductReviewResult>();
+
     CreateMap<Product, ProductResult>()
       .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Id.Value))
       .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Name.Value))
@@ -31,7 +37,13 @@ public class ProductMappingProfile : Profile
       .ForMember(dest => dest.PriceValueInCents, opt => opt.MapFrom(src => src.Price.ValueInCents))
       .ForMember(dest => dest.StockQuantity, opt => opt.MapFrom(src => src.Stock.Quantity))
       .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags))
+      .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories))
       .ForMember(dest => dest.SellerId, opt => opt.MapFrom(src => src.SellerId))
       .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images));
+
+    CreateMap<GetResult<ProductReview>, ProductReviewsResult>()
+      .ForMember(dest => dest.ProductReviews, opt => opt.MapFrom(src => src.Items))
+      .ForMember(dest => dest.TotalCount, opt => opt.MapFrom(src => src.TotalItems))
+      .ForMember(dest => dest.TotalFetchedCount, opt => opt.MapFrom(src => src.TotalItemsFetched));
   }
 }
--- Source/Ecommerce.Application/Common/Mapping/UserMappingProfile.cs
+++ Source/Ecommerce.Application/Common/Mapping/UserMappingProfile.cs
@@ -13,5 +13,13 @@ public class UserMappingProfile : Profile
       .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Value))
       .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.Value))
       .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.Value));
+
+    CreateMap<User, ProductReviewUserResult>()
+      .ForMember(d => d.Id, o => o.MapFrom(s => s.Id.Value))
+      .ForMember(d => d.Fullname, o => o.MapFrom(s => $"{s.FirstName.Value} {s.LastName.Value}"))
+      .ForMember(
+        d => d.ProfileImageUrl,
+        o => o.MapFrom(s => s.ProfileImage != null ? s.ProfileImage.Url : null)
+      );
   }
 }
