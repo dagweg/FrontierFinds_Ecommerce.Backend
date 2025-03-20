8f5f68ff5534d3340932c4622ee9a002c7f758c4
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-20 21:27:54 +0300

    feat: add ProductReviewUserResult class for product review user data

---
+++ Source/Ecommerce.Application/UseCases/Users/Common/ProductReviewUserResult.cs
@@ -0,0 +1,8 @@
+namespace Ecommerce.Application.UseCases.Users.Common;
+
+public class ProductReviewUserResult
+{
+  public required string Id { get; set; }
+  public required string Fullname { get; set; }
+  public required string? ProfileImageUrl { get; set; }
+}
