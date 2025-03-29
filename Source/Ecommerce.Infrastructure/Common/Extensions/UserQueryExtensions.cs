86d3bd5a2940fd843085fe7b2f6a14fce043a0a8
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-29 17:20:53 +0300

    feat: add UserQueryExtensions class for user-related query extensions

---
+++ Source/Ecommerce.Infrastructure/Common/Extensions/UserQueryExtensions.cs
@@ -0,0 +1,9 @@
+using Ecommerce.Application.Common.Extensions;
+using Ecommerce.Application.Common.Models;
+using Ecommerce.Domain.ProductAggregate;
+using Ecommerce.Domain.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Common.Extensions;
+
+public static class UserQueryExtensions { }
