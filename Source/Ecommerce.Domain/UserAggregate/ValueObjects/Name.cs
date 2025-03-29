d6447b4747e2253d5e04a5dff8784c63a6faecf5
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-03-29 17:20:38 +0300

    feat: add MIN_LENGTH and MAX_LENGTH constants to Name value object for length validation

--- Source/Ecommerce.Domain/UserAggregate/ValueObjects/Name.cs
+++ Source/Ecommerce.Domain/UserAggregate/ValueObjects/Name.cs
@@ -1,5 +1,7 @@
 namespace Ecommerce.Domain.UserAggregate.ValueObjects;
 
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.AccessControl;
 using Ecommerce.Domain.Common.Models;
 
 public sealed class Name : ValueObject
@@ -7,6 +9,12 @@ public sealed class Name : ValueObject
   public string Value { get; set; }
 
   public static Name Empty => new(string.Empty);
+
+  [NotMapped]
+  public const int MIN_LENGTH = 50;
+
+  [NotMapped]
+  public const int MAX_LENGTH = 500;
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
   private Name() { }
