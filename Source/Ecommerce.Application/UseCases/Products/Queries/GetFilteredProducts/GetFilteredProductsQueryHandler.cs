532ea535b9bb8c06c00ca9473047542e91954c06
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-26 02:44:51 +0300

    feat: enhance GetFilteredProductsQueryHandler to utilize Elasticsearch for product retrieval

--- Source/Ecommerce.Application/UseCases/Products/Queries/GetFilteredProducts/GetFilteredProductsQueryHandler.cs
+++ Source/Ecommerce.Application/UseCases/Products/Queries/GetFilteredProducts/GetFilteredProductsQueryHandler.cs
@@ -1,7 +1,13 @@
 using AutoMapper;
+using Ecommerce.Application.Common.Defaults;
+using Ecommerce.Application.Common.Errors;
 using Ecommerce.Application.Common.Interfaces.Persistence;
+using Ecommerce.Application.Common.Interfaces.Providers.Search.Elastic;
 using Ecommerce.Application.Common.Models;
+using Ecommerce.Application.Common.Models.Search.Elastic;
+using Ecommerce.Application.Common.Models.Search.Elastic.Documents;
 using Ecommerce.Application.UseCases.Products.Common;
+using Ecommerce.Domain.ProductAggregate.ValueObjects;
 using FluentResults;
 using MediatR;
 
@@ -11,10 +17,16 @@ public class GetFilteredProductsQueryHandler
   : IRequestHandler<FilterProductsQuery, Result<ProductsResult>>
 {
   private readonly IProductRepository _productRepository;
+  private readonly IElasticSearch<ProductDocument> _elasticSearch;
   private readonly IMapper _mapper;
 
-  public GetFilteredProductsQueryHandler(IProductRepository productRepository, IMapper mapper)
+  public GetFilteredProductsQueryHandler(
+    IProductRepository productRepository,
+    IMapper mapper,
+    IElasticSearch<ProductDocument> elasticSearch
+  )
   {
+    _elasticSearch = elasticSearch;
     _productRepository = productRepository;
     _mapper = mapper;
   }
@@ -24,10 +36,36 @@ public class GetFilteredProductsQueryHandler
     CancellationToken cancellationToken
   )
   {
-    var products = await _productRepository.GetFilteredProductsAsync(
-      request,
-      request.PaginationParameters ?? new PaginationParameters()
+    var productsFromElastic = await _elasticSearch.SearchAsync(
+      new ElasticSearchFilterParams() { Index = ElasticIndices.ProductIndex }
     );
+
+    GetProductsResult products;
+
+    if (productsFromElastic is null || !productsFromElastic.Any()) // If nothing found in elastic search fallback to db search
+    {
+      Console.WriteLine("No products found in ElasticSearch, falling back to DB search.");
+      products = await _productRepository.GetFilteredProductsAsync(
+        request,
+        request.PaginationParameters ?? new PaginationParameters()
+      );
+    }
+    else // If found in elastic search, use that data
+    {
+      Console.WriteLine("Products found in ElasticSearch, using that data.");
+      var productsFromDb = await _productRepository.BulkGetByIdAsync(
+        productsFromElastic.Select(p => ProductId.Convert(Guid.Parse(p.Id))).ToList()
+      );
+
+      products = new GetProductsResult()
+      {
+        MinPriceValueInCents = productsFromElastic.Min(p => p.PriceValueInCents),
+        MaxPriceValueInCents = productsFromElastic.Max(p => p.PriceValueInCents),
+        Items = productsFromDb.Values,
+        TotalItems = productsFromElastic.Count(),
+        TotalItemsFetched = productsFromElastic.Count(),
+      };
+    }
 
     return new ProductsResult
     {
