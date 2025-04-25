e8c01258af9981d86ab05e04fab71cb7b508b18d
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-26 02:44:06 +0300

    feat: add ElasticIndices class for managing Elasticsearch index constants

---
+++ Source/Ecommerce.Application/Common/Defaults/ElasticIndices.cs
@@ -0,0 +1,15 @@
+namespace Ecommerce.Application.Common.Defaults;
+
+public static class ElasticIndices
+{
+  public const string ProductIndex = "products";
+  public const string CategoryIndex = "categories";
+  public const string BrandIndex = "brands";
+  public const string OrderIndex = "orders";
+  public const string UserIndex = "users";
+  public const string CartIndex = "carts";
+  public const string WishlistIndex = "wishlists";
+  public const string ReviewIndex = "reviews";
+  public const string AddressIndex = "addresses";
+  public const string PaymentMethodIndex = "payment-methods";
+}
