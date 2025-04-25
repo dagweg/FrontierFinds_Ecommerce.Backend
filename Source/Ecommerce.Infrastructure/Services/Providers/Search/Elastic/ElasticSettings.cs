b0aff02a280009b2f53ae59103ddcfac465df566
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-26 02:45:25 +0300

    feat: add ElasticSettings class for Elasticsearch configuration

---
+++ Source/Ecommerce.Infrastructure/Services/Providers/Search/Elastic/ElasticSettings.cs
@@ -0,0 +1,7 @@
+namespace Ecommerce.Infrastructure.Services.Providers.Search.Elastic;
+
+public class ElasticSettings
+{
+  public const string SectionName = "ElasticSettings";
+  public string ConnectionString = null!;
+}
