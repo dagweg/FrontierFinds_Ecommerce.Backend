7a8555e0ca33f1e3a8d961bb129b1541d5dacfe4
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-30 13:43:17 +0300

    feat: add Dump and PascalToCamelCase extension methods to StringExtensions

--- Source/Ecommerce.Application/Common/Extensions/StringExtensions.cs
+++ Source/Ecommerce.Application/Common/Extensions/StringExtensions.cs
@@ -17,4 +17,17 @@ public static class StringExtensions
     }
     return null;
   }
+
+  public static void Dump(this string value)
+  {
+    Console.WriteLine(value);
+  }
+
+  public static string PascalToCamelCase(this string value)
+  {
+    if (string.IsNullOrEmpty(value) || char.IsLower(value[0]))
+      return value;
+
+    return char.ToLowerInvariant(value[0]) + value[1..];
+  }
 }
