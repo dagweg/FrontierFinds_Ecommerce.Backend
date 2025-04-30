d5d048380491e941b62392a0ad2b4a1585fb2125
Dagmawi Wegayehu <dagtef2@gmail.com>
2025-04-30 13:45:07 +0300

    feat: update ICollectionQueryResult to implement IDataSourceTrackable interface

--- Source/Ecommerce.Application/UseCases/Common/Interfaces/ICollectionQueryResult.cs
+++ Source/Ecommerce.Application/UseCases/Common/Interfaces/ICollectionQueryResult.cs
@@ -1,7 +1,15 @@
+using Ecommerce.Application.Common.Models.Enums;
+
 namespace Ecommerce.Application.UseCases.Common.Interfaces;
 
-public interface ICollectionQueryResult
+public interface ICollectionQueryResult : IDataSourceTrackable
 {
   int TotalCount { get; set; }
   int TotalFetchedCount { get; set; }
+}
+
+public interface IDataSourceTrackable
+{
+  DataSourceType_Debug DataSourceType { get; set; }
+  string DataSourceId { get; set; }
 }
